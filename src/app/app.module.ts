400672f7a35f9ccf4f8229b87e09c5deec8e5cfa
1462510625@qq.com <fan123456>
2017-07-17 16:07:54 +0800

    增加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {FoundPage} from "../pages/found/found";
+import {MycookPage} from "../pages/mycook/mycook";
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FoundPage,
+    MycookPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FoundPage,
+    MycookPage
   ],
   providers: [
     StatusBar,
