883a1663c01c5dcd0346d0913aa6027fe8756680
a247304496@126.com <sunyujie1234>
2017-07-17 20:01:51 +0800

    孙玉杰修改

--- src/pages/mycook/mycook.ts
+++ src/pages/mycook/mycook.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MycookPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-mycook',
   templateUrl: 'mycook.html',
