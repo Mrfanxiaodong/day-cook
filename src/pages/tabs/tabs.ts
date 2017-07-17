7124045b7d27ba36f12d5eced33aa303df44a52d
lishaozhan <2351221785@qq.com>
2017-07-17 16:45:42 +0800

    李少展修改

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import {MycookPage} from "../mycook/mycook";
+import {FoundPage} from "../found/found";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +14,8 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = FoundPage;
+  tab5Root = MycookPage;
 
   constructor() {
 
