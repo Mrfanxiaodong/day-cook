e643b1d0a7190017c380a3b57f8bfe14d79cf0f1
1462510625@qq.com <fan123456>
2017-07-19 09:55:46 +0800

    主页

---
+++ src/cook.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class CookService{
+  constructor(public http:Http) {}
+
+  all(){
+    return this.http.get('http://localhost:3000/dds')
+      .map(res=>res.json().data)
+  };
+  alld_more(){
+  return this.http.get('http://localhost:3000/dpc')
+    .map(res=>res.json().data)
+  }
+  alld_two(){
+    return this.http.get('http://localhost:3000/d_two')
+      .map(res=>res.json().data)
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { NavController,Slides } from 'ionic-angular';
+import {MycookPage} from "../mycook/mycook";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
+import {CookService} from "../../cook.service";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  cookarr = [];
+  d_morepic=[];
+  d_two=[];
+  constructor(public navCtrl: NavController,
+              public cs:CookService
+  ) {}
 
-  constructor(public navCtrl: NavController) {
+  ngOnInit(): void {
+  this.cs.all().subscribe(data=>{
+    this.cookarr = data;
+  });
+  this.cs.alld_more().subscribe(data=>{
+      this.d_morepic = data;
+    })
+    this.cs.alld_two().subscribe(data=>{
+      console.log(data);
+      this.d_two = data;
+    })
 
   }
 
+
+  // go(){
+  //   this.navCtrl.push(MycookPage,{uname:'tom'})
+  // }
+
+
 }
+
