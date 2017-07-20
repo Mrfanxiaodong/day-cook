0d4f734b5b7ecb8284b3eb4e8a440a66cc642ed1
a247304496@126.com <sunyujie1234>
2017-07-20 15:17:23 +0800

    孙玉杰修改4.0

--- src/cook.service.ts
+++ src/cook.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
-
+const url = 'http://localhost:3000/users';
 @Injectable()
 export class CookService{
   constructor(public http:Http) {}
@@ -26,4 +26,15 @@ export class CookService{
     return this.http.post('http://localhost:3000/dds/',{pagNum:num})
       .map(res=>res.json().data)
   };*/
+
+
+  login(){
+    return this.http.get(url)
+      .map(res=>{return res.json().data})
+  }
+
+  register(uname:string,upsd:string){
+    return this.http.post(url,{uname,upsd})
+      .map(res=>{return res.json().data})
+  }
 }
--- src/pages/user-center/user-center.ts
+++ src/pages/user-center/user-center.ts
@@ -1,30 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import {MycookPage} from "../mycook/mycook";
-import {AboutPage} from "../about/about";
-import {HomePage} from "../home/home";
+import {Component,OnInit} from '@angular/core';
+import {NavController,NavParams} from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import {CookService} from "../../cook.service";
 
-/**
- * Generated class for the UserCenterPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-@Component({
+@Component ({
   selector: 'page-user-center',
   templateUrl: 'user-center.html',
 })
-export class UserCenterPage {
+export class UserCenterPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tab: string = 'zc';
+  uname: string;
+  upsd: string;
+
+  ngOnInit(): void {
+
+  }
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public cs: CookService) {
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad UserCenterPage');
-  }
-  back(){
-    this.navCtrl.pop(UserCenterPage)
+    console.log ('ionViewDidLoad UserCenterPage');
 
   }
+
+  //返回函数
+  back() {
+    this.navCtrl.pop (UserCenterPage)
+  }
+
+  //切换类函数
+  switchTab(tab: string) {
+    this.tab = tab;
+  }
+
+  user=[]
+
+  subButton(){
+    if (this.tab == 'zc'){
+      console.log(123);
+      this.cs.register(this.uname,this.upsd).subscribe(data=>{
+      });
+    }else{
+      this.cs.login().subscribe(data=>{
+
+        for (var i=0;i<data.length;i++){
+          console.log(data[i]);
+          console.log(data[i].uname,data[i].upsd);
+          if (this.uname==data[i].uname && this.upsd == data[i].upsd){
+            this.navCtrl.pop (UserCenterPage);
+          }else{
+            console.log('账号或密码错误')
+          }
+        }
+      });
+    }
+  }
+
 }
