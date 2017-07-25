20032cafb3d305b4700b7b521a903bc76abe1228
1445888103 <zcj8836189>
2017-07-25 13:36:53 +0800

    zcj 1.0

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,6 +3,13 @@ import { NavController, NavParams } from 'ionic-angular';
 import {CookService} from "../../cook.service";
 import {SearchonePage} from "../searchone/searchone";
 
+/**
+ * Generated class for the SearchPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html',
